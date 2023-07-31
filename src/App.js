919429b00acca90405485c909be4c915df03491a
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-31 18:25:38 +0300

    Add front reminder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useCallback, useEffect, useReducer, useState } from 'react';
 import './App.css';
 
-function App() {
+const initialState = 1;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment': {
+
+      const newState = state + 1;
+
+      return newState;
+    }
+    case 'decrement': {
+
+      return state - 1;
+    }
+    default: return state;
+  }
+}
+
+function App(props) {
+
+  // const [count, setCount] = useState(1);
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+
+  const decrement = useCallback(() => {
+    const action = {
+      type: 'decrement'
+    }
+
+    dispatch(action)
+  }, [count]);
+
+  const increment = () => {
+    const action = {
+      type: 'increment'
+    }
+
+    dispatch(action)
+  };
+
+  useEffect(function effects() {
+    // componentDidMount , componentDidUpdate
+    console.log('decrement changed')
+    return function clearEffects() {
+      // componentWillUnmount але також перед кожним componentDidUpdate (effects)
+    }
+  }, [decrement])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Current count is {count}</p>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
