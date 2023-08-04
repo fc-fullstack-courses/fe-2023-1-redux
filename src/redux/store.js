1657f040169e60dec3c36684f5c31ef2e55dc5d8
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-04 18:31:37 +0300

    Add toolkit store example

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,20 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from '@redux-devtools/extension';
+import { configureStore } from '@reduxjs/toolkit';
+// import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+// import createSagaMiddleware from 'redux-saga';
+// import { composeWithDevTools } from '@redux-devtools/extension';
 import rootReducer from './reducers';
-import rootSaga from './sagas';
+// import rootSaga from './sagas';
 
-const sagaMW = createSagaMiddleware();
+// const sagaMW = createSagaMiddleware();
 
-const enchancer = applyMiddleware(sagaMW);
+// const enchancer = applyMiddleware(sagaMW);
 
-const store = createStore(rootReducer, composeWithDevTools(enchancer));
+// const store = createStore(rootReducer, composeWithDevTools(enchancer));
 
-sagaMW.run(rootSaga);
+const store = configureStore({
+  reducer: rootReducer
+});
+
+// sagaMW.run(rootSaga);
 
 export default store;
